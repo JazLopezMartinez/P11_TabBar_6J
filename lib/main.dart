41120f1b4b4bb6d9a4cd4caa85e504a0e4372ef1
JAZMIN_LOPEZ_MARTINEZ <a.21308051280495@cbtis128.edu.mx>
2024-02-29 17:08:04 +0000

    actualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,75 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Ejemplo TabBar",
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+        ),
+        home: MiPaginaInicial());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+}
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+    return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Lopez TabBar"),
+            bottom: TabBar(tabs: [
+              Tab(
+                text: "Flores",
+                icon: Icon(Icons.local_florist_outlined),
+              ),
+              Tab(
+                text: "Arreglos",
+                icon: Icon(Icons.grass_sharp),
+              ),
+              Tab(
+                text: "Periodos",
+                icon: Icon(Icons.filter_vintage_rounded),
+              ),
+              Tab(
+                text: "Somos",
+                icon: Icon(Icons.person_outline_rounded),
+              ),
+            ] //Texto icono
+                ),
+          ),
+          body: TabBarView(children: const <Widget>[
+            Center(
+              child: Text("FLORES!",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50)),
+            ),
+            Center(
+              child: Text("ARREGLOS!",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50)),
+            ),
+            Center(
+              child: Text("FLORES DE TEMPORADA!",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50)),
+            ),
+            Center(
+              child: Text("NOSOTROS!",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50)),
+            ),
+          ]),
+        ));
   }
 }
